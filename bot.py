9a10268c6d9fb7126e8485591bb9dceb3248d9cd
danniebgoode <danielcalatayudf@gmail.com>
2025-07-21 10:10:08 +0200

    wake up bot http request

--- bot.py
+++ bot.py
@@ -3,12 +3,16 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Mess
 from dotenv import load_dotenv
 from handlers import start, button_handler, cancel
 from choose_sport import handle_bet_amount_input
+from threading import Thread
+from wake_up_server import run_flask
 
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 if __name__ == "__main__":
+    flask_thread = Thread(target=run_flask, daemon=True)
+    flask_thread.start()
     app = Application.builder().token(TELEGRAM_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("cancel", cancel))
