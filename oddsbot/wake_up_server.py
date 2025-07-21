f2434c1b5b68454fdffc822c75de12d6a56eed16
danniebgoode <danielcalatayudf@gmail.com>
2025-07-21 18:52:34 +0200

    wake up server linted

--- oddsbot/wake_up_server.py
+++ oddsbot/wake_up_server.py
@@ -1,13 +1,19 @@
 from flask import Flask
 
+
 def run_flask():
     app = Flask(__name__)
 
-    @app.route('/wake-up', methods=['GET'])
+    @app.route("/wake-up", methods=["GET"])
     def wake_up():
-        return '<html><body><h1>OK</h1></body></html>', 200, {'Content-Type': 'text/html'}
+        return (
+            "<html><body><h1>OK</h1></body></html>",
+            200,
+            {"Content-Type": "text/html"},
+        )
 
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
+
 
 if __name__ == "__main__":
-    run_flask() 
+    run_flask()
